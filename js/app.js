d374fa17955fc42ace752605358034d22ec639ac
Bhuwan Gautam <bhuwangautam@lftechnology.com>
2015-07-04 02:34:13 +0545

    Fixed the date issue in dashboard

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 'use strict';
 
 angular.module('lms', ['ngRoute'])
-    .controller('MainCtrl', function ($scope, $timeout) {
-        // Build the date object
-        $scope.date = {};
+    .controller('MainCtrl', function ($scope) {
+        // render  google sign in button
+        $scope.renderSignInButton = function () {
+            console.log("inside Main Controller");
+            gapi.signin.render('signInButton', {
+                'callback': $scope.signInCallback,
+                'clientid': '329152280119-ergpn5gjq8hfkf6ru97rfl3dckfkcoph.apps.googleusercontent.com',
+                'requestvisibleactions': 'http://schemas.google.com/AddActivity',
+                'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email',
+                'cookiepolicy': 'single_host_origin',
+                'access_type': 'offline',
+                'grant_type': 'authorization_code',
+                'response_type': 'code'
+            });
+            console.log("rendered button")
+        };
 
-        // Update function
-        var updateTime = function () {
-            $scope.date.raw = new Date();
-            $timeout(updateTime, 1000);
-        }
+        //$scope.renderSignInButton();
 
-        // Kick off the update function
-        updateTime();
     });
